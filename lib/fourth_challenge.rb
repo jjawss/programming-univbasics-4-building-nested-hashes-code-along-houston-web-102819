d58740f49ea027725ecba4c7a4fc866ef803f9b8
David Jaworski <jaws360@gmail.com>
2019-10-16 03:33:26 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,17 +4,15 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
-        "Benvolio" => {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
+      :hero_friends => [{
+          name:"Benvolio",
+          age:"17",
+          attitude: "worried"
         },
         "Mercutio" => {
-          :name => "Mercutio",
-          :age => "18",
-          :attitude => "hot-headed"
+          name:"Mercutio",
+          age:"18",
+          attitude:"hot-headed"
         }
       ]
    }, 
